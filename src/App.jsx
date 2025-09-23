7478402c71af313b64eaf7dcab68522a36938a18
CIAC <CIAC@CIAC2-004.academic.mixcoac.upmx.mx>
2025-09-23 13:00:49 -0600

    nuevoinicio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+import Catlog from "./components/Catlog"
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Catlog></Catlog>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Catlog.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+export default function Catlog(){
+    return(
+        <>
+        <div className='flex flex-wrap justify-center'>
+            <div className='shadow shadow-gray-400 border rounded-2xl w-4/9'>
+                <p>Producto 1</p>
+            </div>
+        </div>
+        </>
+    )
+}
